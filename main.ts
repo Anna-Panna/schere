4700ba700335b3da4b2679635bc80df4923ca0ce
Anna-Panna <anna.m.neumeier@icloud.com>
2024-10-28 10:42:34 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,33 @@
+let zeichen = 0
+input.onGesture(Gesture.Shake, function () {
+    zeichen = randint(1, 3)
+    if (zeichen == 1) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+    if (zeichen == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (zeichen == 3) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+    basic.pause(2000)
+    basic.clearScreen()
+})
