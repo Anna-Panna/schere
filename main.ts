3addd932df6208d3f6f79a742df662dc4391e3b3
Anna-Panna <anna.m.neumeier@icloud.com>
2024-10-28 10:54:15 +0100

    Erweitert

--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
+let punkt = 0
 let zeichen = 0
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    punkt += 1
+    if (punkt == 1) {
+        basic.setLedColor(0x00ff00)
+    } else if (punkt == 2) {
+        basic.setLedColors(0x00ff00, 0x00ff00, 0xffffff)
+    } else if (punkt == 3) {
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        basic.setLedColors(0x00ff00, 0x00ff00, 0x00ff00)
+    } else {
+        basic.turnRgbLedOff()
+    }
+})
 input.onGesture(Gesture.Shake, function () {
     zeichen = randint(1, 3)
     if (zeichen == 1) {
